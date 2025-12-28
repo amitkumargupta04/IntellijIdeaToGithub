53097e4fece99c5d95b29fcec2924fadae7e7a1a
Amit Kumar Gupta <amit1704gupta@gmail.com>
2025-12-28 13:01:22 +0530

    Initial commit - Hello changed extra

--- src/main/java/com/example/Fitness/web/apps/config/Hello.java
+++ src/main/java/com/example/Fitness/web/apps/config/Hello.java
@@ -3,6 +3,6 @@ package com.example.Fitness.web.apps.config;
 public class Hello {
 
     public void testfn(){
-        System.out.println("Hello changed");
+        System.out.println("Hello changed for shortcut key");
     }
 }
